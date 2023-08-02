ae86936d532b4e6597b8cf63dd991096d49ee062
Jack Kallend <Kallendo96@live.co.uk>
2023-08-02 17:24:06 +0100

    feat(facebook pixel): add into layout

--- apps/gk-next/src/app/layout.tsx
+++ apps/gk-next/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Script from 'next/script'
+import Image from 'next/image'
 import { Montserrat, Raleway, Alegreya, Source_Serif_4 } from 'next/font/google'
 
 import '../styles/global.css'
@@ -85,6 +86,26 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 s0.parentNode.insertBefore(s1,s0);
                 })();`}
             </Script>
+            <Script id="facebook" strategy="afterInteractive">
+                {`v!function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window,document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '427561258137519');
+                fbq('track', 'PageView');`}
+            </Script>
+            <noscript>
+                <Image
+                    alt=""
+                    height="1"
+                    width="1"
+                    src="https://www.facebook.com/tr?id=427561258137519&ev=PageView&noscript=1"
+                />
+            </noscript>
             <body id="body" className="mt-[67px] md:mt-[83px] lg:mt-[88px]">
                 <Header />
                 <main>{children}</main>
